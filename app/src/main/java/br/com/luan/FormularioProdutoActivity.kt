05cb2a0d75997e764d7b4e5f01ef386d34043ab1
profmuruk <profluanmuruk@gmail.com>
2024-09-30 08:13:26 -0300

    criando tela de cadastro de produtos

---
+++ app/src/main/java/br/com/luan/FormularioProdutoActivity.kt
@@ -0,0 +1,12 @@
+package br.com.luan
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FormularioProdutoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.formulario_produto_activity)
+    }
+}

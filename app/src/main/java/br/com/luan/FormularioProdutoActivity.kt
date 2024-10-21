358a7df478a5d501a06ec5f4668c17d4605b791a
profmuruk <profluanmuruk@gmail.com>
2024-10-21 10:41:37 -0300

    arquivo de componente do cadastro de produto funcionando

--- app/src/main/java/br/com/luan/FormularioProdutoActivity.kt
+++ app/src/main/java/br/com/luan/FormularioProdutoActivity.kt
@@ -2,11 +2,46 @@ package br.com.luan
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import br.com.luan.model.Produto
+import java.math.BigDecimal
 
 class FormularioProdutoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.formulario_produto_activity)
+
+        val salvarProduto = findViewById<Button>(R.id.botao_salvar)
+
+        salvarProduto.setOnClickListener{
+            val inputNome = findViewById<EditText>(R.id.nome_produto)
+            val nome = inputNome.text.toString()
+
+            val inputDescricao = findViewById<EditText>(R.id.descricao_produto)
+            val descricao = inputDescricao.text.toString()
+
+            val inputValor = findViewById<EditText>(R.id.valor_produto)
+            val textoValor = inputValor.text.toString()
+            /*
+            * converte o valor em texto para bigdecimal caso
+            * o valor não seja branco ou nulo*/
+            val valor = if(textoValor.isBlank()){
+                BigDecimal.ZERO
+            }else{
+                BigDecimal(textoValor)
+            }
+
+            val novoProduto = Produto(
+                nome = nome,
+                descricao = descricao,
+                valor = valor
+            )
+
+            Log.i("Formulario", "o que veio: $novoProduto")
+        }
+
     }
 }
